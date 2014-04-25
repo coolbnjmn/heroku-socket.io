225d3f82a6007a93e3d97467f495fffd53854cf2
Benjamin Hendricks <benjaminhendricks@KirbyMaster.local>
2014-04-24 18:27:30 -0700

    2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('../socket.io')(server);
+var io = require('socket.io').listen(server);
 var port = process.env.PORT || 3000;
 var path = require('path');
 
--- public/main.js
+++ public/main.js
@@ -23,7 +23,7 @@ $(function() {
   var lastTypingTime;
   var $currentInput = $usernameInput.focus();
 
-  var socket = io();
+  var socket = io.connect();
 
   function addParticipantsMessage (data) {
     var message = '';
