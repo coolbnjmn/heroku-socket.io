df4fc605982adf5b7e7b286f51838fa2aa78dc3c
Benjamin Hendricks <benjaminhendricks@KirbyMaster.local>
2014-04-25 17:57:32 -0700

    adding more support

--- index.js
+++ index.js
@@ -5,83 +5,219 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var port = process.env.PORT || 3000;
 var path = require('path');
-
-//server.listen(port, function () {
- // console.log('Server listening at port %d', port);
-//});
-console.log("Listening on port" + port);
-server.listen(port);
+var mongo = require('mongodb');
 
 // Routing
 app.use(express.static(__dirname + '/public'));
 
-require('./routes/index')(app);
-// Chatroom
+
+var uristring = "mongodb://heroku_app24187196:mv1mqgrfmmdi4jhfll5t6j5d01@ds047197.mongolab.com:47197/heroku_app24187196";
+var mongoose = require('mongoose');
+mongoose.connect(uristring);
+
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var flash = require('connect-flash');
+var passport = require('passport') ,
+FacebookStrategy = require('passport-facebook').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
+app.use(cookieParser('secretString'));
+app.use(session({cookie: { maxAge : 1000*60*60*24 }}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+//server.listen(port, function () {
+// console.log('Server listening at port %d', port);
+//});
+console.log("Listening on port" + port);
+server.listen(port);
+
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = new mongoose.Schema({
+                                     image: {
+                                     data: Buffer,
+                                     contentType: String
+                                     },
+                                     local: {
+                                     email : String,
+                                     password: String,
+                                     socket: Object
+                                     },
+                                     facebook : {
+                                     id : String,
+                                     token : String,
+                                     name : String,
+                                     email : String
+                                     }
+                                     });
+
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+var User = mongoose.model('user', userSchema);
+
 
 // usernames which are currently connected to the chat
 var usernames = {};
 var numUsers = 0;
 
 io.on('connection', function (socket) {
-  var addedUser = false;
-
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
-  });
-
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username) {
-    // we store the username in the socket session for this client
-    socket.username = username;
-    // add the client's username to the global list
-    usernames[username] = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
-    // remove the username from global usernames list
-    if (addedUser) {
-      delete usernames[socket.username];
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
+      var addedUser = false;
+      
+      // when the client emits 'new message', this listens and executes
+      socket.on('new message', function (data) {
+                // we tell the client to execute 'new message'
+                socket.broadcast.emit('new message', {
+                                      username: socket.username,
+                                      message: data
+                                      });
+                });
+      
+      // when the client emits 'add user', this listens and executes
+      socket.on('add user', function (username) {
+                // we store the username in the socket session for this client
+                socket.username = username;
+                // add the client's username to the global list
+                usernames[username] = username;
+                ++numUsers;
+                addedUser = true;
+                socket.emit('login', {
+                            numUsers: numUsers
+                            });
+                // echo globally (all clients) that a person has connected
+                socket.broadcast.emit('user joined', {
+                                      username: socket.username,
+                                      numUsers: numUsers
+                                      });
+                });
+      
+      // when the client emits 'typing', we broadcast it to others
+      socket.on('typing', function () {
+                socket.broadcast.emit('typing', {
+                                      username: socket.username
+                                      });
+                });
+      
+      // when the client emits 'stop typing', we broadcast it to others
+      socket.on('stop typing', function () {
+                socket.broadcast.emit('stop typing', {
+                                      username: socket.username
+                                      });
+                });
+      
+      // when the user disconnects.. perform this
+      socket.on('disconnect', function () {
+                // remove the username from global usernames list
+                if (addedUser) {
+                delete usernames[socket.username];
+                --numUsers;
+                
+                // echo globally that this client has left
+                socket.broadcast.emit('user left', {
+                                      username: socket.username,
+                                      numUsers: numUsers
+                                      });
+                }
+                });
       });
-    }
-  });
-});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback (err, db) {
+        User.findOne({'local.email' : "bhendricks@pacbell.net"}, function(err, user) {
+                     console.log('made it here');
+                });
+                // yay!
+        });
+
+mongo.connect(uristring, function(err, db) {
+              if(err) throw err;
+              console.log('Connected to database');
+              
+              });
+
+
+
+passport.use('local-signup', new LocalStrategy({
+                                               usernameField : 'email',
+                                               passwordField : 'password',
+                                               passReqToCallback : true
+                                               }, function(req, email, password, done) {
+                                               process.nextTick(function() {
+                                                                User.findOne({ 'local.email' : email }, function(err, user) {
+                                                                             if(err) return done(err);
+                                                                             if(user) {
+                                                                             return done(null, false, req.flash('signupMessage', 'That email has already been taken.'));
+                                                                             } else {
+                                                                             var newUser = new User();
+                                                                             newUser.local.email = email;
+                                                                             newUser.local.password = newUser.generateHash(password);
+                                                                             newUser.save(function(err) {
+                                                                                          if(err)
+                                                                                          throw err;
+                                                                                          return done(null, newUser);
+                                                                                          });
+                                                                             }
+                                                                             });
+                                                                });
+                                               }));
+
+passport.use('local-login', new LocalStrategy({
+                                              usernameField : 'email',
+                                              passwordField : 'password',
+                                              passReqToCallback : true
+                                              }, function(req, email, password, done) {
+                                              console.log('in local-login');
+                                              User.findOne({'local.email' : email}, function(err, user) {
+                                                           if(err)
+                                                           return done(err);
+                                                           console.log('finded');
+                                                           if(!user)
+                                                           return done(null, false, req.flash('loginMessage', 'No user found.'));
+                                                           if(!user.validPassword(password))
+                                                           return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+                                                           
+                                                           return done(null, user);
+                                                           });
+                                              }));
+
+passport.serializeUser(function(user, done) {
+                       done(null, user.id);
+                       });
+
+passport.deserializeUser(function(id, done) {
+                         User.findById(id, function(err, user) {
+                                       done(err, user);
+                                       });
+                         });
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+                   message: err.message,
+                   error: {}
+                   });
+        });
+
+app.use(function(req, res, next) {
+        req.db = db;
+        next();
+        });
+
+var routes = require('./routes/index');
+app.use('/', routes);
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,49 @@
+var passport = require('passport');
 
-module.exports = function(app) {
-  console.log('trying to load chat');
-  app.get('/chat', function(req, res, next) {
-    res.render('chat');
-  });
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+           res.render('index', { title: 'GymBudUcla' });
+           });
+
+
+router.get('/chat', function(req, res, next) {
+        res.render('chat');
+        });
+
+router.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+        });
+
+router.get('/login', function(req, res) {
+           console.log('login page');
+           res.render('login', {user: req.user, message: req.flash('loginMessage') });
+           });
+
+router.post('/login', passport.authenticate('local-login', {
+                                             successRedirect : '/',
+                                             failureRedirect : '/login',
+                                             failureFlash: true
+}));
+
+router.get('/signup', function(req, res) {
+           res.render('signup', {user : req.user, message: req.flash('signupMessage') });
+           });
+
+router.post('/signup', passport.authenticate('local-signup', {
+                                             successRedirect : '/',
+                                             failureRedirect : '/signup',
+                                             failureFlash: true
+                                             }));
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        console.log('isLoggedIn');
+        return next();
+    }
+    res.redirect('/');
 }
+
+module.exports = router;
